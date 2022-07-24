754c812502ffb5d8708f060cf77528aeb3a93508
z-god <1476482763@qq.com>
2022-07-24 14:40:55 +0800

    feat: 处理url方法

---
+++ src/helpers/util.ts
@@ -0,0 +1,11 @@
+const toString = Object.prototype.toString
+
+export const isArray = Array.isArray
+
+export function isDate(val: any): val is Date {
+  return toString.call(val) === '[object Date]'
+}
+
+export function isObject(val: any): val is Object {
+  return val !== null && typeof val === 'object'
+}
