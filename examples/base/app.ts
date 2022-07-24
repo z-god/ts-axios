378e3b897905fc181599cade2b59190ff1e7856b
z-god <1476482763@qq.com>
2022-07-24 17:28:14 +0800

    feat: headers处理demo

--- examples/base/app.ts
+++ examples/base/app.ts
@@ -76,3 +76,25 @@ axios({
     url: '/base/buffer',
     data: arr
 })
+
+axios({
+    method: 'post',
+    url: '/base/post',
+    headers: {
+        'content-type': 'application/json',
+        'Accept': 'application/json,text/plain,*/*'
+    },
+    data: {
+        a: 1,
+        b: 2
+    }
+})
+
+const paramsString = 'q=URLUtils.searchParams&topic=api'
+const searchParams = new URLSearchParams(paramsString)
+
+axios({
+    method: 'post',
+    url: '/base/post',
+    data: searchParams
+})
