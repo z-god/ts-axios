dcb78391277f497e8ce56c582941e5366ed093aa
z-god <1476482763@qq.com>
2022-07-25 22:50:46 +0800

    feat: 添加错误处理

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,6 +5,7 @@ export interface AxiosRequestConfig {
   params?: any
   headers?: any
   responseType?: XMLHttpRequestResponseType
+  timeout?: number
 }
 
 export type Method =
--- src/xhr.ts
+++ src/xhr.ts
@@ -4,18 +4,26 @@ import { AxiosPromise, AxiosRequestConfig, AxiosResponse } from './types'
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
   return new Promise((resolve, reject) => {
     // 解构获取配置项，并赋予初始值
-    const { data = null, url, method = 'get', headers, responseType } = config
+    const { data = null, url, method = 'get', headers, responseType, timeout } = config
     // 创建request对象
     const request = new XMLHttpRequest()
 
     if (responseType) {
       request.responseType = responseType
     }
+    if (timeout) {
+      request.timeout = timeout
+    }
 
     request.open(method.toUpperCase(), url, true)
 
+    // 接收到响应
     request.onreadystatechange = function handleLoad() {
       if (request.readyState !== 4) {
+        return
+      }
+
+      if (request.status === 0) {
         return
       }
 
@@ -29,7 +37,16 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
         config,
         request
       }
-      resolve(response)
+      handleResponse(response)
+    }
+
+    request.ontimeout = function handleTimeOut() {
+      reject(new Error(`Timeout of ${request.timeout} ms exceeded`))
+    }
+
+    // 网络错误
+    request.onerror = function handleError() {
+      reject(new Error('Network Error'))
     }
 
     Object.keys(headers).forEach(name => {
@@ -41,5 +58,13 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
     })
 
     request.send(data)
+
+    function handleResponse(response: AxiosResponse): void {
+      if (response.status >= 200 && response.status < 300) {
+        resolve(response)
+      } else {
+        reject(new Error(`Request failed with status code ${response.status}`))
+      }
+    }
   })
 }
