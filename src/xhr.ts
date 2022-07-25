08f6e7dd661f207d01fffd526da27d70749149a2
z-god <1476482763@qq.com>
2022-07-25 22:32:54 +0800

    improvement: 解析headers

--- src/xhr.ts
+++ src/xhr.ts
@@ -1,3 +1,4 @@
+import { parseHeaders } from './helpers/headers'
 import { AxiosPromise, AxiosRequestConfig, AxiosResponse } from './types'
 
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
@@ -18,7 +19,7 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
         return
       }
 
-      const responseHeaders = request.getAllResponseHeaders()
+      const responseHeaders = parseHeaders(request.getAllResponseHeaders())
       const responseData = responseType !== 'text' ? request.response : request.responseText
       const response: AxiosResponse = {
         data: responseData,
