cb073280d5a9566f15547bb46db3b87a99afa578
unknown <zengxingui@klicen.com>
2022-08-08 20:48:10 +0800

    feat: 添加自定义转换

--- src/defaults.ts
+++ src/defaults.ts
@@ -1,3 +1,13 @@
+/*
+ * @Author: error: git config user.name && git config user.email & please set dead value or install git
+ * @Date: 2022-08-08 20:12:33
+ * @LastEditors: error: git config user.name && git config user.email & please set dead value or install git
+ * @LastEditTime: 2022-08-08 20:46:50
+ * @FilePath: \ts-axios\src\defaults.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import { transformRequest, transformResponse } from './helpers/data'
+import { processHeaders } from './helpers/headers'
 import { AxiosRequestConfig } from './types'
 
 const defaults: AxiosRequestConfig = {
@@ -9,7 +19,20 @@ const defaults: AxiosRequestConfig = {
     common: {
       Accept: 'application/json, text/plain, */*'
     }
-  }
+  },
+
+  transformRequest: [
+    function(data: any, headers: any): any {
+      processHeaders(headers, data)
+      return transformRequest(data)
+    }
+  ],
+
+  transformResponse: [
+    function(data: any): any {
+      return transformResponse(data)
+    }
+  ]
 }
 
 const methodsNoData = ['delete', 'get', 'head', 'options']
