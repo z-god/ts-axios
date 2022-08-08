6b0164b6decf8f144f4a62c11b891821a177729b
unknown <zengxingui@klicen.com>
2022-08-08 21:16:07 +0800

    feat: 提交测试程序

--- examples/config/app.ts
+++ examples/config/app.ts
@@ -1,39 +1,52 @@
+/*
+ * @Date: 2022-08-08 20:12:33
+ * @LastEditors: error: git config user.name && git config user.email & please set dead value or install git
+ * @LastEditTime: 2022-08-08 21:13:21
+ * @FilePath: \ts-axios\examples\config\app.ts
+ */
 import axios from '../../src/index'
 import qs from 'qs'
+import { AxiosTransFormer } from '../../src/types'
 
-axios.defaults.headers.common['test2'] = 123
-
-axios({
-    url: '/config/post',
-    method: 'post',
-    data: qs.stringify({
-        a: 1
-    }),
-    headers: {
-        test: '321'
-    }
-}).then((res) => {
-    console.log(res.data)
-})
+// axios.defaults.headers.common['test2'] = 123
 
 // axios({
-//   transformRequest: [(function(data) {
-//     return qs.stringify(data)
-//   }), ...(axios.defaults.transformRequest as AxiosTransformer[])],
-//   transformResponse: [...(axios.defaults.transformResponse as AxiosTransformer[]), function(data) {
-//     if (typeof data === 'object') {
-//       data.b = 2
+//     url: '/config/post',
+//     method: 'post',
+//     data: qs.stringify({
+//         a: 1
+//     }),
+//     headers: {
+//         test: '321'
 //     }
-//     return data
-//   }],
-//   url: '/config/post',
-//   method: 'post',
-//   data: {
-//     a: 1
-//   }
 // }).then((res) => {
-//   console.log(res.data)
+//     console.log(res.data)
 // })
+
+axios({
+  transformRequest: [
+    function(data) {
+      return qs.stringify(data)
+    },
+    ...(axios.defaults.transformRequest as AxiosTransFormer[])
+  ],
+  transformResponse: [
+    ...(axios.defaults.transformResponse as AxiosTransFormer[]),
+    function(data) {
+      if (typeof data === 'object') {
+        data.b = 2
+      }
+      return data
+    }
+  ],
+  url: '/config/post',
+  method: 'post',
+  data: {
+    a: 1
+  }
+}).then(res => {
+  console.log(res.data)
+})
 
 // const instance = axios.create({
 //   transformRequest: [(function(data) {
--- src/core/transform.ts
+++ src/core/transform.ts
@@ -1,3 +1,9 @@
+/*
+ * @Date: 2022-08-08 20:48:46
+ * @LastEditors: error: git config user.name && git config user.email & please set dead value or install git
+ * @LastEditTime: 2022-08-08 21:14:45
+ * @FilePath: \ts-axios\src\core\transform.ts
+ */
 import { AxiosTransFormer } from '../types'
 
 export default function transform(
@@ -15,4 +21,6 @@ export default function transform(
   fns.forEach(fn => {
     data = fn(data, headers)
   })
+
+  return data
 }
