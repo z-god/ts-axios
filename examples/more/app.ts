943639f18e771159f99c7af9554c8f771fc35e97
z-god <1476482763@qq.com>
2022-09-16 21:35:04 +0800

    feat: Auth demo

--- examples/more/app.ts
+++ examples/more/app.ts
@@ -83,16 +83,16 @@ uploadEl!.addEventListener('click', e => {
   }
 })
 
-// axios.post('/more/post', {
-//   a: 1
-// }, {
-//   auth: {
-//     username: 'Yee',
-//     password: '123456'
-//   }
-// }).then(res => {
-//   console.log(res)
-// })
+axios.post('/more/post', {
+  a: 1
+}, {
+  auth: {
+    username: 'Yee',
+    password: '123456'
+  }
+}).then(res => {
+  console.log(res)
+})
 
 
 // axios.get('/more/304').then(res => {
